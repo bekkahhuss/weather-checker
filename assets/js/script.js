ea5cda5c928c62701fadd9d255b7a857f166a54c
bekkahhuss <bekkahhuss@gmail.com>
2022-05-24 23:05:22 -0400

    established api key and made variables

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var weatherData = document.querySelector(".weather-data");
+var weekday = [
+    "Sun", 
+    "Mon", 
+    "Tues", 
+    "Wed", 
+    "Thurs", 
+    "Fri", 
+    "Sat"
+];
+var month = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+];
+
+var apiKey = "0d14ac98a1874c4037b8dd76c4fc2bd7";
