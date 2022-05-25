392f15182eec7f72afe73b24b1e313444b4dd202
bekkahhuss <bekkahhuss@gmail.com>
2022-05-25 00:27:46 -0400

    fetch request for city based on user input. Generates weather data based on lat/lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,55 @@ var month = [
 ];
 
 var apiKey = "0d14ac98a1874c4037b8dd76c4fc2bd7";
+
+var nameInput = document.getElementById('city-search-input');
+
+document.querySelector('form.city-form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    getCity(nameInput.value);    
+});
+
+var getCity = function(userInput) {
+    // var userInput = document.querySelector("#city-search-input").value;
+    // document.querySelector("#city-search-input").value = "";
+    
+    var apiCityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=5&appid=" + apiKey;
+        fetch(apiCityUrl).then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    var cityObj = {Name: data[0].name, Lat: data[0].lat, Long: data[0].lon};
+                    console.log(cityObj);
+                    console.log(userInput);
+                    getWeatherData(cityObj);
+                }
+            )}
+                else {
+                    alert('Error: City Not Found');
+                }
+        })
+};
+
+var getWeatherData = function(input) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + input.Lat + "&lon=" + input.Long + "&exclude=hourly,daily&appid=" + apiKey;
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    // displaySearchResult(data);
+                    console.log(data);
+                });
+            } else {
+                alert("Error: " + response.statusText)
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to OpenWeather");
+        });
+};
+
+
+getCity();
+getWeatherData();
